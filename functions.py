f1780004bacb4a3cc03769c221c8f3e7995ebec5
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2023-08-24 11:54:45 -0400

    Blacken python code

--- functions.py
+++ functions.py
@@ -1,38 +1,48 @@
 import discord
 
 
-async def setup_category(interaction: discord.Interaction,
-                         new_category: str) -> dict:
+async def setup_category(
+    interaction: discord.Interaction, new_category: str
+) -> dict:
     try:
         for category in interaction.guild.categories:
             if category.name == new_category:
-                return {'success': True,
-                        'message': f'Category "**{new_category}**" exists.'}
+                return {
+                    "success": True,
+                    "message": f'Category "**{new_category}**" exists.',
+                }
 
         await interaction.guild.create_category(new_category)
-        return {'success': True,
-                'message': f'Category "**{new_category}**" created.'}
+        return {
+            "success": True,
+            "message": f'Category "**{new_category}**" created.',
+        }
 
     except Exception as Error:
-        return {'success': False,
-                'message': f'Failed to create "**{new_category}**": {Error}'}
+        return {
+            "success": False,
+            "message": f'Failed to create "**{new_category}**": {Error}',
+        }
 
 
 def count_match_channels(interaction: discord.Interaction) -> int:
     channel_count = 0
     for channel in interaction.guild.channels:
-        if channel.name.startswith('match-'):
+        if channel.name.startswith("match-"):
             channel_count += 1
 
     return channel_count
 
 
 async def create_staff_role(interaction: discord.Interaction) -> dict:
-    staff_role = discord.utils.get(interaction.guild.roles,
-                                   name="Tournament Staff")
+    staff_role = discord.utils.get(
+        interaction.guild.roles, name="Tournament Staff"
+    )
     if staff_role:
-        return {'success': True,
-                'message': 'Role "**Tournament Staff**" already exists.'}
+        return {
+            "success": True,
+            "message": 'Role "**Tournament Staff**" already exists.',
+        }
 
     permissions = discord.Permissions(
         read_messages=True,
@@ -42,21 +52,25 @@ async def create_staff_role(interaction: discord.Interaction) -> dict:
         manage_channels=True,
         manage_roles=True,
         create_instant_invite=True,
-        embed_links=True
+        embed_links=True,
     )
 
     try:
-        await interaction.guild.create_role(name="Tournament Staff",
-                                            permissions=permissions)
-        return {'success': True,
-                'message':
-                    'Role "**Tournament Staff**" created successfully.'}
+        await interaction.guild.create_role(
+            name="Tournament Staff", permissions=permissions
+        )
+        return {
+            "success": True,
+            "message": 'Role "**Tournament Staff**" created successfully.',
+        }
 
     except discord.Forbidden:
-        return {'success': False,
-                'message':
-                    'Error: Missing perms to create "**Staff**" role.'}
+        return {
+            "success": False,
+            "message": 'Error: Missing perms to create "**Staff**" role.',
+        }
     except Exception as Error:
-        return {'success': False,
-                'message':
-                    f'Error creating "**Tournament Staff**" role: {Error}'}
+        return {
+            "success": False,
+            "message": f'Error creating "**Tournament Staff**" role: {Error}',
+        }
